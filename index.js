4837fd2d1d816323266cf6787fb1d9d704628cbc
PraderioM <PraderioM@Gmail.com>
2018-10-19 22:30:22 +0200

    changed paths.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/static/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(3000, function () {
