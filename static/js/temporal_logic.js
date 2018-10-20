55f4a3c7d5d00099366c5b4f11c3af082938daa9
PraderioM <PraderioM@Gmail.com>
2018-10-20 15:52:57 +0200

    push

--- static/js/temporal_logic.js
+++ static/js/temporal_logic.js
@@ -3,17 +3,34 @@ function register_event(func) {
     CURRENT_MOVEMENTS.push([delta_t, func])
 }
 
-function register_run() {
+function register_run(Cube) {
     REGISTERED_MOVEMENTS.push(CURRENT_MOVEMENTS);
-    CURRENT_MOVEMENTS = []
+    CURRENT_MOVEMENTS = [[Cube.x, Cube.y]]
 }
 
-function identity(Object) {
-    pass
-}
-
-function run_future(Object, scene){
+function run_future(Cube, scene) {
+    //If there are NO registered movements then you are a noob and you have lost.
     if (REGISTERED_MOVEMENTS.length === 0) {
-        printCombo(0, 0, 0, 'LOSER', scene, 0xffe131)
+        printCombo(0, 0, 0, 'LOSER', scene, 0xffe131);
+        return 0
+    }
+
+    // Get last registered movement.
+    var last_movements = REGISTERED_MOVEMENTS.pop();
+
+    // Get initial position.
+    var pos = last_movements[0];
+    var x = pos[0];
+    var y = pos[1];
+    Cube.mat.position.set(x, y, 0);
+
+    var aux;
+    var time_delta;
+    var func;
+    for (var i = 1; i < last_movements.length; i++) {
+        aux = last_movements[i];
+        time_delta = aux[0];
+        func = aux[1];
+        setTimeout(func, time_delta, Cube);
     }
 }
