15567141f1b4fc7806a75e3c784e8c118a8da01a
taras-laptop <tarasyaremabcn@gmail.com>
2018-10-20 16:04:02 +0200

    Damn node is working baes

--- static/js/levels/level3.js
+++
@@ -1,9 +0,0 @@
-const level3 = {
-	"platforms": [[-2, 2, 12], [16, -5, 10], [-2, 6, 10]],
-	"portal": [20, -10],
-	"exit": [21, -5],
-	"init": [-2, 2],
-	"hor_bar": [[5,-3,2]],
-	"ver_bar": null,
-	"spikes_lever":null
-};
