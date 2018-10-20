8b92ac3ed6dac6a2375ea33c9258d609435f4113
m315 <marc.jovanibertran@gmail.com>
2018-10-20 22:40:01 +0200

    lol

--- static/js/levels/level0.js
+++
@@ -1,9 +0,0 @@
-const level0 = {
-	"platforms": [[0, 1, 10]],
-	"portals": null,
-	"exit": [5, 1],
-	"init": [0,1],
-	"hor_bar": null,
-	"ver_bar": null,
-	"spikes_lever": null
-};
--- static/js/levels/level1.js
+++
@@ -1,9 +0,0 @@
-const level1 = {
-	"platforms": [[-2, 2, 12], [16, -5, 10], [24, 12, 10]],
-	"portals": [[21, -5]],
-	"exit": [28, 12],
-	"init": [-2, 2],
-	"hor_bar": [[16,-3,2]],
-	"ver_bar": null,
-	"spikes_lever":null
-};
