fa3334ddfe6c506146feb622ccc61fc51846fbf5
taras-laptop <tarasyaremabcn@gmail.com>
2018-10-20 15:23:35 +0200

    DAMN G

---
+++ static/js/levels/level2.js
@@ -0,0 +1,9 @@
+const level2 = {
+	"platforms": [[-2, 2, 12], [16, -5, 10], [16, 6, 10]],
+	"portal": [20, -10],
+	"exit": [21, -5],
+	"init": [-2, 2],
+	"hor_bar": [[5,-3,2]],
+	"ver_bar": null,
+	"spikes_lever":null
+};
